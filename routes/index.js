4da4b73e8584a2dc68eb474b6bfa65668f025e25
Thu-Tammy <bigbabychicken@gmail.com>
2020-12-17 10:07:34 -0700

    fix index.js

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ var Item = require('../models/items');
 router.get('/', function(req, res, next) {
   Item.find(function (err, items) {
     if (err) console.log(err)
-  res.render('index', { title: 'Under Body Skin', items: items });
+
+    res.render('index', { title: 'Under Body Skin', items: items });
+  })
 });
+
 router.get('/myviews', function(req, res, next) {
   res.render('myviews');
 });
@@ -16,6 +19,5 @@ router.post('/', function(req, res, next) {
   console.log(req.body.item);
   res.redirect('/');
 });
-
 
 module.exports = router;
